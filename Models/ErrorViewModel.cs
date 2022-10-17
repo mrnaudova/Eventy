431eb8faed2888c07197f660596e873e93f9c2cd
student <student@DESKTOP-0JTC4MN>
2022-10-17 10:17:19 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Miroslava_s_Eventy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
